cec6631bb0711c816d3b5ecb79b31cd8ab77c796
Renzo Viscio <89817504+Zonorg@users.noreply.github.com>
2023-06-26 19:13:03 -0300

    Update main

--- JuniorsBank/src/main/main.java
+++ JuniorsBank/src/main/main.java
@@ -7,7 +7,7 @@ public class main {
 
     public static void main(String[] args) {
         Cajero cajero1 = new Cajero();
-        cajero1.menuDelCajero();
+        cajero1.menuDelCajero(); 
     }
 
 }

45c8dc59e255f3e3754601b89e8b1dd0dba0279b
gastonfr24 <gastonfr24@gmail.com>
2023-06-27 17:40:15 -0300

    capa-de-datos

--- BankApp/Models/postgres.py
+++ BankApp/Models/postgres.py
@@ -1,11 +1,15 @@
 import psycopg2
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def connect():
     try:
         conn =psycopg2.connect(
                 database="BankApp",
                 user="postgres",
-                password="xxxxxx",
+                password=os.getenv('PASSWORD_DB'),
                 host="127.0.0.1",
                 port="5432",
             )
